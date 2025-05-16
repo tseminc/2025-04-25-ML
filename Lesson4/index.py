5ab7d9f1265601ab6f5fa49c2f2d114f324aa4c1
tseminc <tseminc.ios@gmail.com>
2025-05-16 13:38:16 +0800

    2nd submit 05-16-2025

--- Lesson4/index.py
+++ Lesson4/index.py
@@ -1,1 +1,20 @@
-print("Hello! Flask!")
+from flask import Flask
+
+# To execute this service, use :  flask --app index run
+#To use debug mode, use :  flask --app index run --debug
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html.jinja2")
+
+@app.route("/user")
+def Hello():
+    return "<H1>Hello, World!</H1><br><H2>This is my second page</H2>"
+
+@app.route("/product")
+def hello3():
+    return "<H1>Hello, World!</H1><br><H2>This is my third page</H2>"
+
