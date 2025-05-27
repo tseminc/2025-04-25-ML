6e1ae3db53b53e3537c905bd3e935c26b35e2c74
tseminc <tseminc.ios@gmail.com>
2025-05-27 17:09:44 +0800

    1st submit

---
+++ Lesson6/index.py
@@ -0,0 +1,38 @@
+from flask import Flask
+
+# To execute this service, use :  flask --app index run
+#To use debug mode, use :  flask --app index run --debug
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html.jinja2")
+
+@app.route("/classes")
+def classes():
+    name = "Aleck"
+    weekdays = [ "星期一", "星期二", "星期三", "星期四", "星期五" ,"星期六" ,"星期日" ]
+    return render_template("classes.html.jinja2", name = name, weekdays = weekdays)
+
+@app.route("/news")
+def news():
+    return render_template("news.html.jinja2")
+
+@app.route("/traffic")
+def traffic():
+    return render_template("traffic.html.jinja2")
+
+@app.route("/contactus")
+def contactus():
+    return render_template("contactus.html.jinja2")
+
+@app.route("/user")
+def Hello():
+    return "<H1>Hello, World!</H1><br><H2>This is my second page</H2>"
+
+@app.route("/product")
+def hello3():
+    return "<H1>Hello, World!</H1><br><H2>This is my third page</H2>"
+
